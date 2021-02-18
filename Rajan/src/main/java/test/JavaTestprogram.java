4df7eb9abcebfa51f0ace1098aec36a74a8b5e10
Asalta <Asalta@MaRi-Asalta>
2021-02-18 12:47:45 +0530

    Java Sample program

---
+++ Rajan/src/main/java/test/JavaTestprogram.java
@@ -0,0 +1,25 @@
+package test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaTestprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://www.leafground.com/pages/sorttable.html");
+		driver.close();
+		
+		
+	}
+
+}
