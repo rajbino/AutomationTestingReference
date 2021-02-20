c514c56c2d39eeeffd5789c0ef9a7548a5475dea
Asalta <Asalta@MaRi-Asalta>
2021-02-20 13:29:45 +0530

    Update java programs

--- Rajan/src/main/java/test/JavaTestprogram.java
+++ Rajan/src/main/java/test/JavaTestprogram.java
@@ -3,26 +3,38 @@ package test;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class JavaTestprogram {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	ChromeDriver driver;
 		
-System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-		
-		
-		ChromeDriver driver = new ChromeDriver();
+        @BeforeTest
+        public void beforeTest() {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		 driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
+        }
+		
+		@Test
+		public void openUrl()
+		{
 		driver.get("http://www.leafground.com/pages/sorttable.html");
 		
 		System.out.println("Success");
 		
+		}
+		
+		@AfterTest
+		public void afterTest() {
 		driver.close();
 		
-		
-	}
-
+		}
+	
+	
+	
 }
